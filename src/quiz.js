d29900c82b5b718def3e3ce739813cba4561cd76
Aliakbar Torbati <alitorbati1368@gmail.com>
2024-02-19 17:03:06 +0100

    day01

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,45 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const x = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[x]] = [
+        this.questions[x],
+        this.questions[i],
+      ];
+    }
+    return this.questions;
+  }
 
-    // 6. hasEnded()
+
+
+  checkAnswer(answer) {
+    if (answer) {
+      return this.correctAnswers++;
+    }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex == this.questions.length) {
+      return true;
+    }
+  }
 }
