fbdb5b19b6f9b40eb18fda159961232a36ef6ae0
onurcangulpak <onurcangulpak@gmail.com>
2024-02-25 16:35:36 +0100

    done

--- src/question.js
+++ src/question.js
@@ -1,22 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    // 1. constructor (text, choices, answer, difficulty)
-    constructor(text,choices,answer,difficulty){ 
+  // YOUR CODE HERE:
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
-    this.difficulty =difficulty; 
-}
+    this.difficulty = difficulty;
+  }
   // 2. shuffleChoices()
-shuffleChoices(){
-    for(let i=this.choices.length - 1; i>0; i--){ 
-        const x = Math.floor(Math.random()* (i+1));
-        [this.choices[i],this.choices[x]] = [this.choices[x], this.choices[i]];
-}
-return this.choices;
-
-}
-
-
-
+  shuffleChoices() {
+    for (let i = this.choices.length - 1; i > 0; i--) {
+      const x = Math.floor(Math.random() * (i + 1));
+      [this.choices[i], this.choices[x]] = [this.choices[x], this.choices[i]];
+    }
+    return this.choices;
+  }
 }
